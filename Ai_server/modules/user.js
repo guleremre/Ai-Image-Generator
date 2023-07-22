4a12e52393a6993d3b62fd52b5249ecafcb36dfd
Emre Guler <mfbg1985@gmail.com>
2023-07-22 21:28:14 +0300

    username added

--- Ai_server/modules/user.js
+++ Ai_server/modules/user.js
@@ -3,11 +3,19 @@ const connection = require("./connection");
 
 const schema = new mongoose.Schema(
   {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     email: {
       type: String,
+      required: true,
+      unique: true,
     },
     password: {
       type: String,
+      required: true,
     },
   },
   { timestamps: true }
