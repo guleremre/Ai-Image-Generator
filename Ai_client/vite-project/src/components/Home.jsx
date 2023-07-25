d36c4f1f28df92484ef82166029480a92d556e7f
Emre Guler <mfbg1985@gmail.com>
2023-07-25 04:36:57 +0300

    working but refreshing

--- Ai_client/vite-project/src/components/Home.jsx
+++ Ai_client/vite-project/src/components/Home.jsx
@@ -4,24 +4,27 @@ import axios from "axios";
 const url = "http://127.0.0.1:7860/sdapi/v1/txt2img";
 
 function Home() {
-  // const [prompt, setPrompt] = useState();
-  // const [negativePrompt, setNegativePrompt] = useState();
-  // const [image, setImage] = useState();
-  // const [loading, updateLoading] = useState();
+  const [prompt, setPrompt] = useState();
+  const [negative_prompt, setNegativePrompt] = useState();
+  const [img, setImg] = useState([]);
+  const [loading, updateLoading] = useState();
 
-  const makeRequest = async () => {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "*/*",
-    };
-    const data = {
-      prompt: "boy,animated, 8k, volumetric lightning",
-      negative_prompt:
-        "nude, sketches, (worst quality, bad quality, child, cropped:1.4) ((monochrome)), ((grayscale)), (bad-hands-5:1.0), (badhandv4:1.0), (easynegative:0.8), (bad-artist-anime:0.8), (bad-artist:0.8), (bad_prompt:0.8), (bad-picture-chill-75v:0.8), (bad_prompt_version2:0.8), (bad_quality:0.8)",
-    };
+  const onSubmit = async () => {
+    // const headers = {
+    //   "Content-Type": "application/json",
+    //   Accept: "*/*",
+    // };
     try {
-      const response = await axios.post(url, data, { headers });
+      const response = await axios.post(
+        url,
+        { prompt, negative_prompt }
+        // { headers }
+      );
       console.log("Response:", response.data.images[0]);
+     // updateLoading(false);
+      const base64 = response.data;
+      setImg(base64);
+      console.log(img);
       // Handle the response data here
     } catch (error) {
       console.error("Error:", error.message);
@@ -29,7 +32,7 @@ function Home() {
     }
   };
   useEffect(() => {
-    makeRequest();
+    onSubmit();
   }, []);
 
   // const onSubmit = async () => {
@@ -39,12 +42,11 @@ function Home() {
   //       .post(url, { prompt, negativePrompt })
   //       .then((response) => {
   //         console.log(response.json());
-  //         setImage(response.data);
+  //         setImg(response.data);
   //         console.log(image);
   //         updateLoading(false);
   //         console.log(updateLoading);
   //       });
-
   //     console.log("response.data");
   //   } catch (error) {
   //     console.error(error);
@@ -53,44 +55,44 @@ function Home() {
   // };
 
   return (
-    <form>
-      {" "}
-      <h1>hi</h1>
-      {/* <img src="`data:image/jpeg;base64,${imageBase64}`" alt="" /> <br /> */}
-      {/* <textarea
-        type="text"
-        placeholder="prompt"
-        onChange={(e) => {
-          setPrompt(e.target.value);
-        }}
-        width={"350px"}
-      />
-      <br />
-      <textarea
-        type="text"
-        placeholder="negative prompt"
-        onChange={(e) => {
-          setNegativePrompt(e.target.value);
-        }}
-      />
-      <br />
-      <button
-        type="submit"
-        onClick={(e) => {
-          onSubmit();
-        }}
-      >
-        Generate Img
-      </button> */}
-      <button
-        type="submit"
-        onClick={(e) => {
-          makeRequest();
-        }}
-      >
-        Generate Img
-      </button>
-    </form>
+    <>
+      <form>
+        {" "}
+        <h1>Ai Image Generator</h1>
+        <textarea
+          type="text"
+          placeholder="prompt"
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
+        />
+        <br />
+        <textarea
+          type="text"
+          placeholder="negative prompt"
+          onChange={(e) => {
+            setNegativePrompt(e.target.value);
+          }}
+        />
+        
+        <br />
+        <button
+          type="submit"
+          onClick={(e) => {
+            onSubmit();
+          }}
+        >
+          Generate Img
+        </button>
+      </form>
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : img ? (
+          <img src={`data:image/png;base64,${img}`} />
+        ) : null}
+      </div>
+    </>
   );
 }
 
--- Ai_client/vite-project/src/components/Test.jsx
+++ Ai_client/vite-project/src/components/Test.jsx
@@ -10,8 +10,8 @@ const Test = () => {
   const [loading, updateLoading] = useState();
 
   const generate = async (prompt) => {
-    updateLoading(true);
-    const result = await axios.get(url);
+   
+    const result = await axios.get(url,);
     updateImage(result.data);
     updateLoading(false);
   };
@@ -19,11 +19,11 @@ const Test = () => {
   return (
     <form>
       {" "}
-      <h1>hi</h1>
+      <h1>test</h1>
       {/* <img src="`data:image/jpeg;base64,${imageBase64}`" alt="" /> <br /> */}
       <input
         type="text"
-        placeholder="prompt"
+        placeholder="((best quality)), ((masterpiece)), ((realistic)), vintage Afro-Caribbean woman, elegant attire, 1950s fashion, radiant smile, confident stance, cultural pride, (oil painting:1.2),  vivid colors, nostalgic background, authentic vintage feel, (portrait composition:1.3), (high-resolution:1.2)"
         onChange={(e) => {
           updatePrompt(e.target.value);
         }}
@@ -31,7 +31,8 @@ const Test = () => {
       <br />
       <input
         type="text"
-        placeholder="negative prompt"
+        placeholder=
+        "nude,3d, cartoon, anime, sketches, (worst quality, bad quality, child, cropped:1.4) ((monochrome)), ((grayscale)),  (bad-hands-5:1.0), (badhandv4:1.0), (easynegative:0.8),  (bad-artist-anime:0.8), (bad-artist:0.8), (bad_prompt:0.8), (bad-picture-chill-75v:0.8), (bad_prompt_version2:0.8),  (bad_quality:0.8)"
         onChange={(e) => {
           setNegativePrompt(e.target.value);
         }}
@@ -46,6 +47,7 @@ const Test = () => {
         {" "}
         Generate Img
       </button>
+      
     </form>
   );
 };
