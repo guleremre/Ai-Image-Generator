4ce9a58d1a095441b8f983e75fbfc65e240a5df4
Emre Guler <mfbg1985@gmail.com>
2023-08-03 18:43:40 +0300

    profile check login added

--- Ai_client/vite-project/src/components/Profile.jsx
+++ Ai_client/vite-project/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import Avatar from "@mui/material/Avatar";
-import { CssVarsProvider } from "@mui/joy/styles";
+
 import Sheet from "@mui/joy/Sheet";
 import Typography from "@mui/joy/Typography";
 import FormControl from "@mui/joy/FormControl";
@@ -10,14 +10,24 @@ import Input from "@mui/joy/Input";
 import Button from "@mui/material/Button";
 import Link from "@mui/joy/Link";
 import { Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
   const [userInfo, setUserInfo] = useState([]);
   const [userId, setUserId] = useState("");
+  // const [navImg, setNavImg] = useState();
 
   console.log(userInfo.imgUrl);
+
+  function checkIfLogged() {
+    const item = localStorage.getItem("token");
+    if (!item) {
+      return navigate("/home");
+    }
+  }
   //to get user id
   async function auth() {
     const url = "http://localhost:4000/user/verify";
@@ -25,13 +35,11 @@ const Profile = () => {
     try {
       const response = await axios.post(url, { token });
       // console.log("response.data", response.data);
-      const settedUserInfo = response.data;
-      const settedUserId = response.data._id;
       const userName = userInfo.username;
       console.log("feyo", userInfo.username);
       console.log("feyoing", userInfo.imgUrl);
-      setUserInfo(settedUserInfo);
-      setUserId(settedUserId);
+      setUserInfo(response.data);
+      setUserId(response.data._id);
 
       console.log("userInfo", userInfo);
       console.log("userInfo userName", userName);
@@ -56,7 +64,6 @@ const Profile = () => {
       function (error, result) {
         if (!error && result && result.event === "success") {
           const avatar = result.info.secure_url;
-          console.log(avatar);
           put(avatar, userId);
         } else if (error) {
           console.error("Error uploading image:", error);
@@ -65,32 +72,22 @@ const Profile = () => {
     );
   }
 
-  // async function post(avatar, userId) {
-  //   // axios.put("http://localhost:4000/user/" + userId , {imgUrl : clouridiray})
-  //   const url = "http://localhost:4000/img/";
-  //   try {
-  //     var response = await axios.post(url, {
-  //       avatar,
-  //       userId, // Add the userId to the request
-  //     });
-  //     console.log("Upload successful!", response.data);
-  //     console.log("postun ki", userId);
-  //   } catch (error) {
-  //     console.error("Error uploading image:", error);
-  //   }
-  // }
   async function put(avatar, userId) {
     try {
       const response = await axios.put(`http://localhost:4000/user/${userId}`, {
         imgUrl: avatar,
       });
-      var imgs = userInfo.imgUrl;
+
+      localStorage.setItem("NavImg", userInfo.imgUrl);
+      const imgtoken = localStorage.setItem("NavImg", userInfo.imgUrl);
+      console.log("imgtoken", imgtoken);
     } catch (error) {
       console.error("Error uploading image:", error);
     }
   }
 
   useEffect(() => {
+    checkIfLogged();
     auth(); // Call the auth function to get the userId
     UploadToWidget(userId);
   }, [userId]);
@@ -108,69 +105,60 @@ const Profile = () => {
       >
         <h1>{`Profile of ${userInfo.username} `}</h1>
       </Stack>
-      <CssVarsProvider>
-        <main>
-          <Sheet
-            className=" profile"
+
+      <main>
+        <Sheet
+          className=" profile"
+          sx={{
+            maxWidth: 300,
+            mx: "left", // margin left & right
+            my: 4, // margin top & bottom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            borderRadius: "sm",
+            boxShadow: "large",
+          }}
+          color="neutral"
+          variant="soft"
+        >
+          <Stack
             sx={{
-              maxWidth: 300,
-              mx: "left", // margin left & right
-              my: 4, // margin top & bottom
-              py: 3, // padding top & bottom
-              px: 2, // padding left & right
+              m: "left",
               display: "flex",
               flexDirection: "column",
-              gap: 2,
-              borderRadius: "sm",
-              boxShadow: "large",
             }}
-            color="neutral"
-            variant="soft"
+            direction="row"
+            spacing={5}
           >
-            <Stack
-              sx={{
-                m: "left",
-                display: "flex",
-                flexDirection: "column",
-              }}
-              direction="row"
-              spacing={5}
-            >
-              <Avatar
-                sx={{ width: 240, height: 240, alignSelf: "center" }}
-                src={userInfo.imgUrl}
-              />
-            </Stack>
-            <Button
-              onClick={() => widgetRef.current.open()}
-              variant="contained"
-              color="primary"
-              size="small"
-            >
-              Update Avatar
-            </Button>
-            <Stack direction="row" spacing={2}></Stack>
-            <Typography level="h4" component="h1">
-              <FormLabel>User Name</FormLabel>
-              <b>{userInfo.username}</b>
-            </Typography>
+            <Avatar
+              sx={{ width: 240, height: 240, alignSelf: "center" }}
+              src={userInfo.imgUrl}
+            />
+          </Stack>
+          <Button
+            onClick={() => widgetRef.current.open()}
+            variant="contained"
+            color="primary"
+            size="small"
+          >
+            Update Avatar
+          </Button>
+          <Stack direction="row" spacing={2}></Stack>
+          <Typography level="h4" component="h1">
+            <FormLabel>User Name</FormLabel>
+            <b>{userInfo.username}</b>
+          </Typography>
 
-            <Typography level="h4" component="h1">
-              <FormLabel>Email</FormLabel>
-              <b>{userInfo.email}</b>
-            </Typography>
-
-            {/* <Typography
-              endDecorator={<Link href="/">log in</Link>}
-              fontSize="sm"
-              sx={{ alignSelf: "center" }}
-            >
-              Do you have an account?
-            </Typography> */}
-          </Sheet>
-          <Sheet className="favorites"></Sheet>
-        </main>
-      </CssVarsProvider>
+          <Typography level="h4" component="h1">
+            <FormLabel>Email</FormLabel>
+            <b>{userInfo.email}</b>
+          </Typography>
+        </Sheet>
+        <Sheet className="favorites"></Sheet>
+      </main>
     </>
   );
 };
