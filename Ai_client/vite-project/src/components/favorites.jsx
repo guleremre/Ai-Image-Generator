04013e56de6bafbdc8d08af6bd903410b7f5edcb
Emre Guler <mfbg1985@gmail.com>
2023-08-14 01:10:26 +0300

    favs rendered

--- Ai_client/vite-project/src/components/favorites.jsx
+++ Ai_client/vite-project/src/components/favorites.jsx
@@ -8,8 +8,12 @@ const Favorites = () => {
   const [user, setUser] = useState("");
   const [userId, setUserId] = useState("");
   const [userInfo, setUserInfo] = useState("");
+  const [user39, setUser39] = useState([]);
+
+  // const [favedImages, setFavedImages] = useState([]);
 
   const token = localStorage.getItem("token");
+  //to get user info
 
   //Delete from favorites
   const handleDeleteFavorite = async (id) => {
@@ -24,49 +28,39 @@ const Favorites = () => {
       console.log(error);
     }
   };
+  // const deneme = console.log(userInfo?.map((item) => item.image));
 
   useEffect(() => {
-    //to get user info
     async function auth() {
       const url = "http://localhost:4000/user/verify";
       const token = localStorage.getItem("token");
       try {
         const response = await axios.post(url, { token });
-        setUserInfo(response.data);
-        console.log("favorites response.data", response.data);
-        console.log("userInfo.favoriteImg", userInfo.favoriteImg);
+
+        setUserInfo((prevUserInfo) => ({ ...prevUserInfo, ...response.data }));
+        setUser39((prevUser39) => ({
+          ...prevUser39,
+          ...response.data.favoriteImg,
+        }));
+        console.log(userInfo);
+        setUserId(response.data._id);
       } catch (error) {
         console.error("Error verifying token:", error);
       }
+
+      console.log(user39);
     }
     auth();
-  }, []);
-
+  }, [userId]);
   return (
     <div>
       <h1>Here is a list of your favorite books</h1>
       <div>
         <ul>
-          {userInfo.favoriteImg&&
-            userInfo.favoriteImg.map((image, i) => (
-              <li key={i}>
-                <div className="card">
-                  <img src={image} />
-                  <p>{prompt}</p>
-                  {/* <img src={book.image} alt={book.name} />
-                  <article>By {book.author}</article>
-                  <h3>{book.name}</h3>
-                  <h3>Rs: {book.price} $</h3>
-                  {token ? <p>Available: {String(book.available)}</p> : ""} */}
-                  <Button
-                    onClick={() => {
-                      handleDeleteFavorite(image._id);
-                    }}
-                    sx={{ mt: "auto" }}
-                  >
-                    Remove Favorite
-                  </Button>
-                </div>
+          {userInfo.favoriteImg &&
+            userInfo.favoriteImg.map((item) => (
+              <li>
+                <img src={item.image} />
               </li>
             ))}
         </ul>
