52916026f011c453778938d7b3b0e952e2f475f7
Emre Guler <mfbg1985@gmail.com>
2023-08-14 02:24:42 +0300

    favs rendered && prestyled

--- Ai_client/vite-project/src/components/favorites.jsx
+++ Ai_client/vite-project/src/components/favorites.jsx
@@ -3,18 +3,14 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const Favorites = () => {
-  const [user, setUser] = useState("");
   const [userId, setUserId] = useState("");
   const [userInfo, setUserInfo] = useState("");
-  const [user39, setUser39] = useState([]);
-
-  // const [favedImages, setFavedImages] = useState([]);
-
   const token = localStorage.getItem("token");
-  //to get user info
-
   //Delete from favorites
   const handleDeleteFavorite = async (id) => {
     console.log(token);
@@ -28,43 +24,62 @@ const Favorites = () => {
       console.log(error);
     }
   };
-  // const deneme = console.log(userInfo?.map((item) => item.image));
 
   useEffect(() => {
+    //to get user info
     async function auth() {
       const url = "http://localhost:4000/user/verify";
       const token = localStorage.getItem("token");
       try {
         const response = await axios.post(url, { token });
-
         setUserInfo((prevUserInfo) => ({ ...prevUserInfo, ...response.data }));
-        setUser39((prevUser39) => ({
-          ...prevUser39,
-          ...response.data.favoriteImg,
-        }));
         console.log(userInfo);
-        setUserId(response.data._id);
       } catch (error) {
         console.error("Error verifying token:", error);
       }
-
-      console.log(user39);
     }
     auth();
   }, [userId]);
+
   return (
     <div>
       <h1>Here is a list of your favorite books</h1>
+
       <div>
-        <ul>
-          {userInfo.favoriteImg &&
-            userInfo.favoriteImg.map((item) => (
-              <li>
-                <img src={item.image} />
+        {userInfo.favoriteImg && (
+          <ul>
+            {userInfo.favoriteImg.map((item) => (
+              <li key={item._id}>
+                <img src={item.image} alt="Image cover" />
+                <p>prompt={item.prompt} </p>
+                <p>negative_prompt={item.negative_prompt} </p>
+                <p>sampler_index={item.sampler_index} </p>
+                <p>cfg_scale={item.steps} </p>
+                <p>cfg_scale={item.cfg_scale} </p>
               </li>
             ))}
-        </ul>
+          </ul>
+        )}
       </div>
+      {/* <Box sx={{ width: 500, height: 450, overflowY: "scroll" }}>
+        <ImageList variant="masonry" cols={3} gap={8}>
+          {userInfo.favoriteImg.map((item) => (
+            <ImageListItem key={item._id}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <p>prompt={item.prompt} </p>
+              <p>negative_prompt={item.negative_prompt} </p>
+              <p>sampler_index={item.sampler_index} </p>
+              <p>cfg_scale={item.steps} </p>
+              <p>cfg_scale={item.cfg_scale} </p>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box> */}
     </div>
   );
 };
